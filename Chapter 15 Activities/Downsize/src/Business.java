595b86b6019c2b9d3bd94dafa9206519cc720609
BavyaChowdavarapu <111458969+BavyaChowdavarapu@users.noreply.github.com>
2023-09-15 13:13:24 -0500

    downsizeLab

--- Chapter 15 Activities/Downsize/src/Business.java
+++ Chapter 15 Activities/Downsize/src/Business.java
@@ -1,10 +1,13 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * Business utility methods.
 */
 public class Business
 {
+
+    
     /**
       * Removes every nth element from the linked list
       *
@@ -13,6 +16,28 @@ public class Business
      */
     public static void downsize(LinkedList<String> employeeNames, int n)
     {
-        ...
+        //creates copy of the employeeNames linked list
+        LinkedList<String> names = new LinkedList<>();
+
+        //creates iterator to go through the linked list with 
+        ListIterator <String> iterator = names.listIterator();
+
+        //creates counter to count number of employee names passed over by iterator
+        //this is the total number of employees in the list 
+        int numEmployees = 0;
+
+        //gets the number of employees in the linked list
+        while(iterator.hasNext()){
+            numEmployees++;
+        }
+
+        
+
+
+
+
+
+
+
     }
 }
