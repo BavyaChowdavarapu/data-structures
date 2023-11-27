3e30ce29d0a5ec0879c4fd86940264e7e010dad1
BavyaChowdavarapu <111458969+BavyaChowdavarapu@users.noreply.github.com>
2023-11-26 21:41:51 -0600

    finished morsecode lab

--- MorseCode/src/MorseCode.java
+++ MorseCode/src/MorseCode.java
@@ -24,6 +24,7 @@ public class MorseCode
         decodeTree = new TreeNode(' ', null, null);  // autoboxing
         // put a space in the root of the decoding tree
 
+        //addSymbol(key, value)
         addSymbol('A', ".-");
         addSymbol('B', "-...");
         addSymbol('C', "-.-.");
@@ -88,64 +89,48 @@ public class MorseCode
      */
     private static void treeInsert(char letter, String code)
     {
-        //if the position held by "root" is null ---------------------------------------done 
-
-        //if the value of the root is null (size = 0)
-        if (codeMap.size() == 0){
-            //makes the root a space 
-            codeMap.put(null, " ");
-        }
-
         //sets the value of the child of the previous node to this current node -----------------------------done
 
-        //boolean to use in the while loop
-        boolean placed = false;
-
+        
         //object used to traverse the tree
-        TreeNode root =  (TreeNode)decodeTree.getValue();
-
-        //node to put into the tree 
-        TreeNode curr = new TreeNode(letter, null, null);
-
+        TreeNode root = decodeTree; 
+        
         //counter to keep track of the character index in the code 
         int count = 0;
         char ch;
 
         //while loop keeps running as long as the count is less than the string's length 
-        //and the treenode has not been placed 
-        while (!placed && count < (code.length())){
-            //gets the character at the index in the code 
+        while (count < code.length()){
+            //gets the character at the index in the code
             ch = code.charAt(count);
-
+            
             //if the next character in the code is a dot
-            if (ch == DOT){
+            if(ch == DOT){
                 //checks if the left child is null
-                if (root.getLeft() != null){
-                    root = root.getLeft();
+                if(root.getLeft() == null){
+                    //makes the null spot into an empty node
+                    root.setLeft(new TreeNode(null));
                 }
-
-                //if the left child position is null
-                root.setLeft(curr);
-                placed = true;
-            }
-
-            //if the next character in the code is a dash 
-            if (ch == DASH){
-                //checks if the right postitoin is null
-                if (root.getRight() != null){
-                    root = root.getRight();
-                }
+                //sets root to the empty node
+                root = root.getLeft();
+            } else {
+                //if the next character in the code is a dash 
                 
-                //if the right position child is null
-                root.setRight(curr);
-                placed = true;
+                //checks if the right child is null
+                if(root.getRight() == null){
+                    //makes the null spot into an empty node 
+                    root.setRight(new TreeNode(null));
+                }
+                //sets root to the empty node 
+                root = root.getRight();
             }
-
-            //iterated the count to go to the next character in the code 
+            
+            //iterating the while loop
             count++;
         }
-
         
+        //setting the value of the final position of root to the letter 
+        root.setValue(letter);
     }
 
     /**
@@ -156,38 +141,34 @@ public class MorseCode
      */
     public static String encode(String text)
     {
-        StringBuffer morse = new StringBuffer(400);
-
-        //use .append(char ch) here 
-
-        //counter to go through the indices of the text with 
-        int count = 0;
-
-        //char to hold the character in the text
-        char ch;
-
+        StringBuffer morse = new StringBuffer(400); 
         
-        //while loop keeps running as long as the index values are valid 
-        while (count < (text.length())){
-            ch = text.charAt(count);
-
+        //string to hold the code of each letter of the text
+        String code; 
+        
+        //int to hold counter for while loop
+        int count = 0;
+    
+        //while loop iterates as long as counter is a valid character index
+        while (count < text.length()){
             //if the character is a space 
-            if (ch == ' '){
-                morse.append(ch);
-            }
-
-            //if the character is not one in the decoding tree
-            if (codeMap.get(ch) == null){
-                System.out.println("(character " + ch + " is not a valid input)");
-            }
-            //------------------------------------------------------------------------------finish from here down 
+            if(text.charAt(count) == ' '){ 
+                morse.append(" "); 
+            } 
             
-            //if the character is in the decoding tree 
-
-
+            //getting the uppercase version of the letter from the codemap
+            code = codeMap.get(text.toUpperCase().charAt(count)) + " ";
             
+            //if the code is null, append a space 
+            if(code.equals("null ")){
+                code = "  ";
+            }
+            morse.append(code);
+            
+            //iterating the while loop
+            count++;
         }
-
+        
         return morse.toString();
     }
 
@@ -197,17 +178,64 @@ public class MorseCode
      * transferred directly into text.
      * Returns the plain text message.
      */
-    public static String decode(String morse)
-    {
-        StringBuffer text = new StringBuffer(100);
-
-        /*
-            !!! INSERT CODE HERE
-        */
-
+    public static String decode(String morse) {
+        StringBuffer text = new StringBuffer(700); 
+        
+        //object to traverse the treeMap with 
+        TreeNode decodeThis = decodeTree;
+        
+        //counter for index in the morse code text
+        int count = 0;
+        
+        //char to hold each character in the morse code 
+        char ch;
+        //while loop iterates as long as count is a valid index of the code 
+        while(count < morse.length())
+        {
+            //sets ch to each character 
+            ch = morse.charAt(count); 
+            
+            //if the character is a dot 
+            if (ch == DOT)
+            {
+                //checks if the left child of the current position is null
+                if (decodeThis.getLeft() == null)
+                {
+                    //this character is not in the map 
+                    break;
+                }
+            
+                //reassigns decodeThis to the left child
+                decodeThis = decodeThis.getLeft();
+            } else if (ch == DASH) //if the character is a dash 
+            {
+                //checks if the right child of the current position is null
+                if (decodeThis.getRight() == null)
+                {
+                    //this character is not in the map
+                    break;
+                }
+                
+                //reassigns decodeThis to the right child
+                decodeThis = decodeThis.getRight(); 
+            } else 
+            {
+                //if the next character is not a dot or dash 
+                
+                //appends the text with the letter translation of the code
+                text.append(decodeThis.getValue()); 
+                decodeThis = decodeTree;
+            }
+            
+            //iterating the while loop
+            count++;
+        }
+        
         return text.toString();
     }
+    
 }
+
 
 /**
  * BTreePrinter class courtesy of Karen Ge (@karenge1)
