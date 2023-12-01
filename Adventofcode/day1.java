cd4cae045e165a676ef51198483e724ac98467c9
BavyaChowdavarapu <111458969+BavyaChowdavarapu@users.noreply.github.com>
2023-12-01 12:57:36 -0600

    react files download

---
+++ Adventofcode/day1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class line {
+    public String currentLine;
+    public File file;
+    public int totalVal;
+    public int currVal;
+
+    //constructor
+    public line(){
+        currentLine = null;
+        file = null;
+    }
+
+    //set file method
+    public void setFile(File n){
+        file = n;
+    }
+
+    
+    
+}
---
+++ Adventofcode/day1test.java
@@ -0,0 +1,9 @@
+package Adventofcode;
+
+public class day1test {
+    public static void main (String[] args){
+        line myLine = new line();
+    
+        myLine.setFile(new File ("linetest.txt"));
+    }
+}
