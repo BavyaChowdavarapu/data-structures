4c959adf3117b6a6ca0bceb3d480c9bb0205aa57
BavyaChowdavarapu <111458969+BavyaChowdavarapu@users.noreply.github.com>
2023-12-01 13:17:54 -0600

    advent of code

--- Adventofcode/day1test.java
+++
@@ -1,9 +0,0 @@
-package Adventofcode;
-
-public class day1test {
-    public static void main (String[] args){
-        line myLine = new line();
-    
-        myLine.setFile(new File ("linetest.txt"));
-    }
-}
