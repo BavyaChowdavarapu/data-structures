520e48c0153bb073dab1765c72e30deb83dada4b
BavyaChowdavarapu <111458969+BavyaChowdavarapu@users.noreply.github.com>
2023-12-04 13:06:51 -0600

    calculator project complete

--- nodejsfolder/src/index.js
+++ nodejsfolder/src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./component/App";
 import "./index.css";
-import "gh-fork-ribbon.css";
+import "./gh-fork-ribbon.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
